c9c34eb13f05936c49606b12355adeade98e5444
jobkung3d <crystan_job.a@hotmail.com>
2019-06-13 20:19:25 +0700

    notebook 6-13-19

--- src/component/card.js
+++ src/component/card.js
@@ -8,21 +8,36 @@ class CardElement extends Component{
                 <Card className="shadow-sm rounded-0">
                     <CardBody>
                         <div className="crd-bar text-uppercase clearfix">
-                            <p className="float-left mb-1" style={{color:'#6666'}}>Project</p>    
-                            <p className="float-right mb-1">18 Mins Ago</p>
+                            <p className="float-left mb-2" style={{color:'#6666'}}>Project</p>    
+                            <p className="float-right mb-2">18 Mins Ago</p>
                         </div>
                         <div className="crd-body">
-                          <h5 className="mt-0">Webdesign</h5>
-                          <div className="crd-body-tag">
-                              <div className="tag rounded p-3">Tag</div>
-                          </div>
-                          <div className="crd-body-noti my-4">
-                              <div className="row">
-                                  <div className="col-4">test</div>
-                                  <div className="col-4">test</div>
-                                  <div className="col-4">test</div>
-                              </div>
-                          </div>
+                            <h5 className="mt-0 mb-2">Webdesign</h5>
+                            <div className="crd-body-tag">
+                              <div className="tag rounded px-3 py-2 d-inline-block text-uppercase">Tag</div>
+                            </div>
+                            <div className="crd-body-noti my-4">
+                                <div className="row">
+                                    <div className="col-md-6 mb-3">
+                                        <div className="crd-sm border rounded">
+                                            <div className="crd-sm-number px-2 py-1"><h2 className="mb-0">1200</h2></div>
+                                            <div className="crd-sm-title text-center">Assign</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-3">
+                                        <div className="crd-sm border rounded">
+                                            <div className="crd-sm-number px-2 py-1"><h2 className="mb-0">1200</h2></div>
+                                            <div className="crd-sm-title text-center">Assign</div>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6 mb-3">
+                                        <div className="crd-sm border rounded">
+                                            <div className="crd-sm-number px-2 py-1"><h2 className="mb-0">1200</h2></div>
+                                            <div className="crd-sm-title text-center">Assign</div>
+                                        </div>
+                                    </div>
+                                </div>                             
+                            </div>
                         </div>
                         <div className="crd-footer">
 
