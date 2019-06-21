63ea6bdf5286d5930097ba80de38f5cb148f7557
jobkung3d <crystan_job.a@hotmail.com>
2019-06-22 02:03:35 +0700

    notebook update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import reducer  from './reducer';
+import reducers  from './reducers';
 
 
 const store = createStore(reducers);
